6f8d9ddb0b25d1c4aeaff1d8493210115a90fa80
Alex Raul Santo Araujo <06562980143@caltins.local>
2020-09-02 14:21:21 -0300

    Otimizando WebSockets com uWebSocket para uma melhor performance da aplicação

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const uws = require('uws');
 var app = express();
 // passa o express para o http-server
 var http = require('http').Server(app);
@@ -6,6 +7,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static('.'))
 // sempre que o socketio receber uma conexão vai devoltar realizar o broadcast dela
+io.ws = new uws.Server({ perMessageDeflate: false });
 io.on('connection', function(socket){   
   socket.on('chat message', function(msg){
     io.emit('connections',socket.server.eio.clientsCount  )  
