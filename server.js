7737f9aff1b7ee3e2e05f8db46b14a6bc3f977a0
Alex Raul Santo Araujo <06562980143@caltins.local>
2020-09-02 13:40:19 -0300

    small chat with bulma css and socketio

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+var app = express();
+// passa o express para o http-server
+var http = require('http').Server(app);
+// passa o http-server par ao socketio
+var io = require('socket.io')(http);
+app.use(express.static('.'))
+// sempre que o socketio receber uma conexão vai devoltar realizar o broadcast dela
+io.on('connection', function(socket){   
+  socket.on('chat message', function(msg){
+    io.emit('connections',socket.server.eio.clientsCount  )  
+    io.emit('chat message', msg);
+  });
+});
+ // inicia o servidor na porta informada, no caso vamo iniciar na porta 3000
+http.listen(3000, function(){
+  console.log('Servidor rodando em: http://localhost:3000');
+});
